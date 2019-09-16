0813bff6e073be29e1899be23f55b3ec75d65509
DanielGeraldino <danielguerra@ucl.br>
2019-09-15 22:47:41 -0300

    Começando a colocar peças no tabuleiro

--- Xadrez-console/Tabuleiro/Peca.cs
+++ Xadrez-console/Tabuleiro/Peca.cs
@@ -6,5 +6,13 @@
         public Cor cor { get; protected set; }
         public int qteMovimento { get; protected set; }
         public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            this.posicao = null;
+            this.tabuleiro = tab;
+            this.cor = cor;
+            this.qteMovimento = 0;
+        }
     }
 }
